1e4bce366821e8826f6f519b0cbf691ed37712cb
qeshi <6067421+qeshi@users.noreply.github.com>
2019-01-29 11:08:08 +0100

    first commit

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to subtoken-indexer
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ src/subtoken_indexer/core.clj
@@ -0,0 +1,37 @@
+(ns subtoken-indexer.core
+  (:gen-class)
+  )
+
+(defn foo
+  "I don't do a whole lot."
+  [x]
+  (println x "Hello, World!"))
+
+
+
+(def words ["akrobat" "akrobater" "robottekniker" "maskintekniker"])
+
+(defn update-gap [gap chars]
+  (if (= gap (count chars))
+    1
+    (inc gap)
+    )
+  )
+
+(defn word-segmenter
+  ([word]
+   (word-segmenter word (char-array word) 1 [] )
+   )
+  ([word chars gap acc]
+   (if (empty? chars)
+     acc
+     (let [
+           new-gap (update-gap gap chars)
+           new-chars (if (= gap (count chars)) (rest chars) chars)
+           new-acc (conj acc [(take gap chars) word])
+           ]
+       (recur word new-chars new-gap new-acc)
+       )
+     )
+   )
+  )
